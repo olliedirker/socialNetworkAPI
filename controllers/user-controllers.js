14b2010affba469c2211a488638bc2d4967e184e
olliedirker <olliedirker@gmail.com>
2022-10-12 12:14:39 -0500

    user cont done

---
+++ controllers/user-controllers.js
@@ -0,0 +1,73 @@
+const { User } = require("../models");
+//user controller starts here 
+
+const userController = {
+
+  //gets all the users
+  getUsers(req, res) {
+    User.find({})
+      .then((userData) => res.json(userData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //gets a single user by id
+  getUserByID({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((userData) => res.json(userData))
+      .catch((err) => res.status(400).json(err));
+  },
+  //for adding users
+  addUser({ body }, res) {
+    User.create(body)
+      .then((userData) => res.json(userData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+
+        res.json(userData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+
+        res.json(userData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { friends: params.friendID } },
+      { runValidators: true, new: true }
+    )
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  deleteFriend({ params }, res) {},
+};
+
+module.exports = userController;
