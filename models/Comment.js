2e8f3975eec7fb6ea42743f038d408c45b26ef89
olliedirker <olliedirker@gmail.com>
2022-10-12 12:33:08 -0500

    comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,39 @@
-const Comment = require('./Comment');
-const User = require('./User');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const reactionSchema = require('./Reaction');
+
+const CommentSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            validate: [({ length }) => length > 0 && length <= 280, 'Comments can only be between 1 and 280 characters long!']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+);
 
 
-module.exports = {Comment, User};
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+const Comment = model('Comment', Comment);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Comment = require('./Comment');
+const User = require('./User');
+
+
+module.exports = {Comment, User};
